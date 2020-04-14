6f3e24a6616f865bb6880ff13805c19a9f76eaf5
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-04-14 08:45:54 +0700

    update callback

--- callbackInReactJs.js
+++ callbackInReactJs.js
@@ -5,20 +5,38 @@ class Biografi extends Component {
     name: "nama saya adalah ..."
   };
 
+  //hasil 1
   myFirstName = callBack => {
     setTimeout(() => {
       callBack("muhamad");
+    }, 500);
+  };
+  //hasil2
+  middleName = cb => {
+    setTimeout(() => {
+      cb("rahmat");
+    }, 500);
+  };
+  //hasil3 adalah callback gabungan hasil1 dan hasil2
+  lastName = cb2 => {
+    setTimeout(() => {
+      cb2("setiawan");
     }, 500);
   };
 
   changeName = () => {
     this.myFirstName(hasil => {
       let first = hasil;
-      let middle = "rahmat";
-      let last = "setiawan";
-      const myName = `${first} ${middle} ${last}`;
-
-      this.setState({ name: myName });
+      this.middleName(hasil2 => {
+        let middle = hasil2;
+        this.lastName(hasil3 => {
+          let last = hasil3;
+          const myName = `${first} ${middle} ${last}`;
+          this.setState({
+            name: myName
+          });
+        });
+      });
     });
   };
 
